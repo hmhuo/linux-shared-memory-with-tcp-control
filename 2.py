f8ea54a58ccbfa6c0459baa4b9bab361922a853f
hmhuo <gin_huo@hotmail.com>
2017-06-21 00:33:31 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,21 +1,25 @@
 # -*- coding:utf-8 -*-
-import socket
+from socket import *  
+import time
+HOST = '10.0.0.164'  
+PORT = 8000  
+BUFSIZ = 128  
+ADDR = (HOST, PORT)  
+  
+#创建客户端UDP套接字  
+udpClient = socket(AF_INET, SOCK_DGRAM)  
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #创建一个socket
 
-s.connect(('127.0.0.1', 8000))                       #建立连接
 
-while True:                                           #接受多次数据
+data='232222211100'
 
-    data = input('请输入要发送的数据：')                 #接收数据
+udpClient.sendto(data,ADDR)
+# time.sleep(3)
 
-    if data == 'quit':                                #如果为'quit',则退出
-        break
 
-    s.send(data.encode())                             #发送编码后的数据
-
-    print(s.recv(1024).decode('utf-8'))               #打印接收到的大写数据
-
-s.send(b'quit')                                       #放弃连接
-
-s.close()                                             #关闭socket
+# while True:  
+#     data = raw_input('>')  
+#     if not data:  
+#         break  
+# #向服务器端发送数据  
+#     udpClient.sendto(data,ADDR)  
