6c0a12086c4f6adb0ef66e0b7298044308194fa1
hmhuo <gin_huo@hotmail.com>
2017-06-21 00:31:14 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -5,26 +5,42 @@
     
 #define BUF_SIZE 1024     
 #define MYKEY 25     
+
+
+#define TEXT_SZ 2048
+
+
+struct shared_use_st {
+    int speed;
+    int dir;
+    char some_text[TEXT_SZ];
+};  
+
+
 int  main()    
 {    
     int shmid;    
-    char * shmptr;    
-    
-    if((shmid = shmget(MYKEY,BUF_SIZE,IPC_CREAT)) ==-1)    
+
+    void *shared_memory = (void *)0;
+    struct shared_use_st *shared_stuff;
+
+    if((shmid = shmget(MYKEY,sizeof(shared_stuff),IPC_CREAT)) ==-1)    
     {    
     printf("shmget error!\n");    
     exit(1);    
     }    
     
-    if((shmptr = shmat(shmid,0,0)) == (void *)-1)    
+    if((shared_stuff = shmat(shmid,0,0)) == (void *)-1)    
     {    
     printf("shmat error!\n");    
     exit(1);    
     }    
+
+
     
     while(1)    
     {    
-    printf("string :%s\n",shmptr);    
+    printf("speed:%d,dir:%d, some_text:%s  \n",shared_stuff->speed,shared_stuff->dir,shared_stuff->some_text);    
     sleep(3);    
     }    
     
